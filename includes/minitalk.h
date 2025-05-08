83cd45e4b7c2439a2e79277521a10d5ba8446e9e
Haagphur <52178892+Megamax21@users.noreply.github.com>
2025-05-08 02:29:44 +0200

    Add files via upload

--- includes/minitalk.h
+++ includes/minitalk.h
@@ -3,14 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   minitalk.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ml-hote <ml-hote@student.42nice.fr>        +#+  +:+       +#+        */
+/*   By: ml-hote <ml-hote@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/02 05:30:26 by ml-hote           #+#    #+#             */
-/*   Updated: 2025/05/02 05:32:23 by ml-hote          ###   ########.fr       */
+/*   Created: 2025/05/06 16:54:57 by ml-hote           #+#    #+#             */
+/*   Updated: 2025/05/07 18:06:49 by ml-hote          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>     
-#include <stdlib.h>     
-#include <signal.h>     
-#include <stdarg.h>     
+#ifndef MINITALK_H
+# define MINITALK_H
+# include <unistd.h>
+# include <signal.h>
+# include <stdlib.h>
+# include "../libft/libft.h"
+
+typedef struct s_data
+{
+	struct sigaction	sa;
+	int					c_buffer[8];
+	int					i;
+	int					word_end;
+	int					buf_index;
+	int					buf_size;
+	char				*str;
+}	t_data;
+
+#endif
